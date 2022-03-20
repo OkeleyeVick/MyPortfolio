f9902fe9284e461ecf38949ddd888510a7f1603a
justusofnigeria <horlarmhedey200@gmail.com>
2022-03-20 22:55:50 +0100

    omooo

--- html/portfolio.js
+++ html/portfolio.js
@@ -16,7 +16,6 @@ $(document).ready(function(){
     }
     var timer = setInterval(changeThing, 4000);
 
-    
 
     $(window).on("scroll", function(){
         if($(this).scrollTop() > 800){
@@ -43,4 +42,64 @@ $(document).ready(function(){
     }
     progress();
     $(document).on("scroll", progress);
+
+    var darkModelg =    $("[class='icon-moon-o'] ");
+    var lightModelg = $("[class='icon-sun-o']");
+    var navlinks = document.getElementsByClassName("nav-link");
+    
+    $(darkModelg).on("click", function(){
+        $(this).css({
+            visibility: "hidden"
+        })
+        $(lightModelg).css({
+            visibility: "visible",
+        })
+        $(lightModelg).css({
+            color: "rgb(177, 177, 177)"
+        })
+        $("body").css({
+            backgroundColor: "rgba(0, 0, 0, 0.9)",
+            transition: ".45s ease-in-out"
+        })
+        $(navlinks).add(".aboutMe blockquote").css({
+            color: "#fff"
+        })
+        $("button", ".carousel-dark .carousel-control-prev-icon").css({
+            marginTop:"50px"
+        })
+        $(".socials i").css({
+            backgroundColor: "#fff"
+        })
+        $(".socials i").hover(function(){
+            $(this).css({
+                color: "initial"
+            })
+        })
+        $(".story, h4:not(#Contact h4), p:not(#Contact p), h2:not(span),.info, h3, h5, h6, .languageSkill .span, .pr-skill-name").css({
+            color:"white"            
+        })
+    })
+
+    $(lightModelg).on("click", function(){
+        $(this).css({
+            visibility: "hidden"
+        })
+        $(darkModelg).css({
+            visibility: "visible",
+        })
+        $("body").css({
+            backgroundColor: ""
+        })
+        $(navlinks).add(".aboutMe blockquote").css({
+            color: ""
+        })
+        $(".story, h4:not(#Contact h4), p:not(#Contact p), h2:not(span),.info, h3, h5, h6, .languageSkill .span, .pr-skill-name").css({
+            color:""            
+        })
+        $(".navbar-collapse li:first-child a").css({
+            color: "#fff !important"
+        })
+    })
+
+    // The Progress Bar
 })
